d800dcc7def4b08782565d2c6777615ad09e32be
Sitnascode <sitira2022@outlook.com>
2026-01-27 16:55:00 +0300

    Change base path to '/simple-quiz-app/'
    Update base path for asset loading in Vite config.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // <-- make asset paths relative so the site works when served from a subpath (e.g. GitHub Pages)
   plugins: [react()],
+  base: '/simple-quiz-app/',
 })
